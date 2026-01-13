e0a650f13b588a8787c8ad0c17c43146434283f1
TTRAN70 <tonytran124589@gmail.com>
2026-01-12 20:42:24 -0500

    tcp socket communications between client and server

--- client.cpp
+++ client.cpp
@@ -13,6 +13,14 @@
 #include <string>
 #pragma comment(lib, "Ws2_32.lib")
 
+static void die(const char* message) {
+    std::cout << "failed:" << message << std::endl;
+}
+
+static void msg(const char* message) {
+    std::cout << message << std::endl;
+}
+
 int main() {   
     WSADATA wsaData = {0};
     int iResult = 0;
@@ -24,6 +32,28 @@ int main() {
         printf("WSAStartup failed: %d\n", iResult);
         return 1;
     }  
+
+    SOCKET fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (fd < 0) { die("socket()"); }
+
+    struct sockaddr_in addr = {};
+    addr.sin_family = AF_INET;
+    addr.sin_port = ntohs(1234);        // port
+    addr.sin_addr.s_addr = ntohl(INADDR_LOOPBACK); // wildcard IP 0.0.0.0
+
+    int rv = connect(fd, (const sockaddr*)&addr, sizeof(addr));
+    if (rv) { die("connect()"); }
+
+    char wbuf[] = "hello";
+    send(fd, wbuf, strlen(wbuf), 0);
+
+    char rbuf[64] = {};
+    SSIZE_T n = recv(fd, rbuf, sizeof(rbuf) - 1, 0);
+    if (n < 0) { die("read()"); }
+
+    std::cout << "server says: " << rbuf << std::endl;
+    closesocket(fd);
     WSACleanup();
 
     return 0;
--- server.cpp
+++ server.cpp
@@ -16,6 +16,26 @@
 
 #pragma include(lib, "Ws2_32.lib")
 
+static void die(const char* message) {
+    std::cout << "failed:" << message << std::endl;
+}
+
+static void msg(const char* message) {
+    std::cout << message << std::endl;
+}
+
+static void do_something(SOCKET connfd) {
+    char rbuf[64] = {};
+    SSIZE_T n = recv(connfd, rbuf, sizeof(rbuf) - 1, 0);
+
+    if (n < 0) { msg("read error"); return; }
+
+    std::cout << "client says: " << rbuf << std::endl;
+
+    char wbuf[] = "world";  
+    send(connfd, wbuf, strlen(wbuf), 0);
+}
+
 
 int main() { 
     WSADATA wsaData = {0};
@@ -28,6 +48,35 @@ int main() {
         printf("WSAStartup failed: %d\n", iResult);
         return 1;
     }  
+
+    SOCKET fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (fd < 0) { die("socket()"); }
+
+    int val = 1;
+    setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, (const char*)&val, sizeof(val));
+
+    struct sockaddr_in addr = {};
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(1234);        // port
+    addr.sin_addr.s_addr = htonl(0);    // wildcard IP 0.0.0.0
+    int rv = bind(fd, (const struct sockaddr *)&addr, sizeof(addr));
+    if (rv) { die("bind()"); }
+
+    rv = listen(fd, SOMAXCONN);
+    if (rv) { die("listen()"); }
+
+    while(true) {
+        struct sockaddr_in client_addr = {};
+        socklen_t addrlen = sizeof(client_addr);
+        SOCKET connfd = accept(fd, (struct sockaddr *)&client_addr, &addrlen);
+        if (connfd < 0) { continue; } // error
+
+        do_something(connfd);
+        closesocket(connfd);
+    }
+
+    closesocket(fd);
     WSACleanup();
     return 0;
 }
